a2a4612217475078f47d1d828514ba37ff9ef679
thawn <webmaster@korten.at>
2023-06-18 16:18:59 +0200

    fixed path

--- tests/test.sh
+++ tests/test.sh
@@ -2,6 +2,8 @@
 # shellcheck source=../src/scripts/configuration.sh
 source "$HOME"/.fileserver_to_hpc/configuration.sh
 
-rsync -r --delete test_data "$WFJ_WORKDIR"
+tests_path=$(dirname "$0")
+
+rsync -r --delete "$tests_path"/test_data "$WFJ_WORKDIR"
 
 zip -r "$WFJ_WORKDIR"test_data/workflow.zip "$WFJ_WORKDIR"test_data/workflow
