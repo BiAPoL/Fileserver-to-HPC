ea14ff8c1fedccaa77113a076b7125bd95120011
thawn <webmaster@korten.at>
2023-06-18 18:45:08 +0200

    delete the line containing test_data from watch_for_jobs_done.list

--- tests/test.sh
+++ tests/test.sh
@@ -2,6 +2,9 @@
 # shellcheck source=../fileserver_to_hpc/configuration.sh
 source "$HOME"/.fileserver_to_hpc/configuration.sh
 
+# delete the line containing test_data from watch_for_jobs_done.list
+sed -i '/test_data/d' "$HOME"/.fileserver_to_hpc/watch_for_jobs_done.list
+
 tests_path=$(dirname "$0")
 
 rsync -rv --delete "$tests_path"/test_data "$WFJ_WORKDIR"
