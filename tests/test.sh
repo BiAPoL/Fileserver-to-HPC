5c1d87a4f9ff4d68b480d37ec55447cd9ff7a5a6
thawn <webmaster@korten.at>
2023-06-19 12:10:15 +0200

    moved test workflow to example_wrokflows

--- tests/test.sh
+++ tests/test.sh
@@ -3,12 +3,12 @@
 source "$HOME"/.fileserver_to_hpc/configuration.sh
 
 # delete the line containing test_data from watch_for_jobs_done.list
-sed -i '/test_data/d' "$HOME"/.fileserver_to_hpc/watch_for_jobs_done.list
+sed -i '/for_testing/d' "$HOME"/.fileserver_to_hpc/watch_for_jobs_done.list
 
 tests_path=$(dirname "$0")
 
-rsync -rv --delete "$tests_path"/test_data "$WFJ_WORKDIR"
+rsync -rv --delete "$tests_path"/../example_workflows/for_testing "$WFJ_WORKDIR"
 
-cd "$WFJ_WORKDIR"test_data/ || exit 1
+cd "$WFJ_WORKDIR"for_testing/ || exit 1
 echo "creating workflow.zip"
 zip -r workflow.zip workflow
