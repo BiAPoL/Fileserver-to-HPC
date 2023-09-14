43e3857d1d5173315e09ace16e83bd2b4e69870c
thawn <webmaster@korten.at>
2023-09-14 22:14:54 +0200

    use config['axes'] for prediction

--- example_workflows/n2v/workflow/auto_n2v_predict.py
+++ example_workflows/n2v/workflow/auto_n2v_predict.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     model_dir = data_dir / 'model'
     target_dir = data_dir / "denoised"
     target_dir.mkdir(parents=True, exist_ok=True)
-    
+
     # load config
     config = load_config(data_dir / 'workflow' / 'config.json')
 
@@ -32,24 +32,30 @@ if __name__ == "__main__":
     # load data
     datagen = MyDataGenerator()
     imgs = datagen.load_imgs([source_file], dims=config['axes'])
-    image = imgs[0].squeeze()
+    image = imgs[0]
     print("Image shape: ", image.shape)
 
     # calculate tiling so that the tiles fit into the GPU memory
     # on the A100 GPUs (48GB memory), the maximum tile size is approximately 3 MB
     n_tiles = image.size / (3 * 2**20)
-    tiles = (1, np.ceil(np.sqrt(n_tiles)), np.ceil(np.sqrt(n_tiles)))
+    tiles = np.array(image.shape)
+    tiles[config['axes'].index('X')] = 1
+    tiles[config['axes'].index('Y')] = 1
+    if 'Z' in config['axes']:
+        tiles[config['axes'].index('Z')] = 1
+    n_tiles /= np.prod(tiles)
+    if n_tiles > 1:
+        tiles[config['axes'].index('Y')] = np.ceil(np.sqrt(n_tiles))
+        tiles[config['axes'].index('X')] = np.ceil(np.sqrt(n_tiles))
+    tiles = tuple(tiles)
     print("Tiling image: ", tiles)
 
     # predict
-    pred = model.predict(image, axes='ZYX', n_tiles=tiles)
+    pred = model.predict(image, axes=config['axes'], n_tiles=tiles)
     print("Prediction shape: ", pred.shape)
 
     # save prediction
     target_path = target_dir / (source_file.name + '_N2V.tif')
     print("Saving: ", str(target_path))
-    csbdeep.io.save_tiff_imagej_compatible(target_path, pred.astype(np.float32), 'ZYX')
+    csbdeep.io.save_tiff_imagej_compatible(target_path, pred.astype(np.float32), config['axes'])
     print('Done')
-
-
-    
